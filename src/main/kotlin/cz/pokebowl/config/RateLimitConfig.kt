0ea6a946fc3cd7de2673eee9cf237994ad2b5355
Štěpán Záliš <stepan.zalis@gmail.com>
2025-12-14 10:48:08 +0100

    fix: rate limitting

--- src/main/kotlin/cz/pokebowl/config/RateLimitConfig.kt
+++ src/main/kotlin/cz/pokebowl/config/RateLimitConfig.kt
@@ -7,7 +7,7 @@ class RateLimitConfig {
         const val PROTECTED_RATE_LIMIT = "protected"
 
         const val PROTECTED_MAX_REQUESTS = 40
-        const val GLOBAL_MAX_REQUESTS = 20
+        const val GLOBAL_MAX_REQUESTS = 30
 
         val PROTECTED_REFILL_PERIOD = 60.seconds
 
